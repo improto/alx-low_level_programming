c1936c3297a3534d27bcfa5dff1113c8bfdf1d26
Emmanuel <official.emmanuel001@gmail.com>
2022-09-09 02:50:22 -0700

    Additional tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+/**
+ *main - Prints to standard error
+ (*
+ *Description: Thi prints a statement to standard error)?
+ *Return: One
+ */
+int main(void)
+{
+char txt[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+
+fwrite(txt, 59, 1, stderr);
+return (1); }
