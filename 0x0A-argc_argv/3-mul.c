7dca4d2bb9c96eef84ba5f54e5bfdead59097a62
Emmanuel <official.emmanuel001@gmail.com>
2022-09-30 12:24:52 -0700

    Correct file 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,14 +10,16 @@
  */
 int main(int argc, char *argv[])
 {
-	int a;
+	int a, b, c;
 
 	if (argc != 3)
 	{
 		puts("Error");
 		return (1);
 	}
-	a = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n", a;
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = a * b;
+	printf("%d\n", c);
 	return (0);
 }
