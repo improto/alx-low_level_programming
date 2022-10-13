91dacbd2c4f51dff6f8d42fa9b11122779bef0a7
Emmanuel <official.emmanuel001@gmail.com>
2022-10-13 02:34:36 -0700

    Repair file 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,14 @@
 #include "function_pointers.h"
 
-void print_name(char *name, void (*f)(char *));
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function to print with
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL || f != NULL)
+	if (name != NULL && f != NULL)
 		f(name);
 }
